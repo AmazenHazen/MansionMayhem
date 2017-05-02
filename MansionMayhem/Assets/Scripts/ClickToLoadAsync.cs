6722e686841523a2943c611d67307a5a86352d8d
MAIN\aeh8533 <aehazen@gmail.com>
2017-05-02 15:00:18 -0400

    Added new scene for menus (the level selection one will probably change). Started basic UI menu scripting.

---
+++ MansionMayhem/Assets/Scripts/ClickToLoadAsync.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickToLoadAsync : MonoBehaviour {
+
+    public Slider loadingBar;
+    public GameObject loadingImage;
+
+    private AsyncOperation async;
+
+    public void ClickAsync(int level)
+    {
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }
+
+    IEnumerator LoadLevelWithBar (int level)
+    {
+
+        async = Application.LoadLevelAsync(level);
+
+        while(!async.isDone) // Check to see if the level is completely loaded
+        {
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MansionMayhem/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        // IF WE ARE RUNNING IN A UNITY STANDALONE
+    #if UNITY_STANDALONE
+
+            // Quit the Application
+            Application.Quit();
+    #endif
+
+
+        // IF WE ARE RUNNING IN the unity Editor
+    #if UNITY_EDITOR
+
+            // Stop play mode in Unity
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+
+    }
+}

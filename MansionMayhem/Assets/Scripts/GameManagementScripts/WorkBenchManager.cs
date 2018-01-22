28b24778190486048fbe85989cb773a3f52a1789
AMAZENHAZEN\Alex Hazen <aehazen@gmail.com>
2018-01-22 13:13:52 -0500

    Formated the weapons upgraded the screen more, got the scrolling working. Started a script for switching between the sections.

---
+++ MansionMayhem/Assets/Scripts/GameManagementScripts/WorkBenchManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkBenchManager : MonoBehaviour
+{
+
+    // Attributes
+    public GameObject weaponsPanel;
+    public GameObject trinketsPanel;
+    public GameObject outfitPanel;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void SwitchSection()
+    {
+        // Hide the other Panels
+
+    }
+
+}

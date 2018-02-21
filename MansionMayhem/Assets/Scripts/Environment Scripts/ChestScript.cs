ee55fad448381fc9248ac61b56600a968fe969fe
MAIN\aeh8533 <aehazen@gmail.com>
2018-02-21 10:59:00 -0500

    Level fixes

--- MansionMayhem/Assets/Scripts/Environment Scripts/ChestScript.cs
+++ MansionMayhem/Assets/Scripts/Environment Scripts/ChestScript.cs
@@ -31,36 +31,45 @@ public class ChestScript : MonoBehaviour {
                 // Spawn an item: Currency, heart, heart potion, or bonus
                 int randomItemRoll = Random.Range(0, 100);
 
-                // 30% chance of 10 screws
+                // 30% chance of 75 screws
                 if (randomItemRoll < 25)
                 {
-                    Debug.Log("Chest Contained a 10 Screws");
+                    Debug.Log("Chest Contained a 75 Screws");
+                    GameManager.instance.screws += 75;
                 }
-                // 20% chance of 20 screws
+                // 25% chance of 100 screws
                 else if (randomItemRoll >= 25 && randomItemRoll < 50)
                 {
-                    Debug.Log("Chest Contained a 20 Screws");
+                    Debug.Log("Chest Contained a 100 Screws");
+                    GameManager.instance.screws += 100;
 
                 }
-                // 15% chance of 40 screws
+                // 20% chance of 125 screws
                 else if (randomItemRoll >= 50 && randomItemRoll < 70)
                 {
-                    Debug.Log("Chest Contained a 40 Screws");
+                    Debug.Log("Chest Contained a 125 Screws");
+                    GameManager.instance.screws += 125;
+
                 }
-                // 10% chance of high health object
-                else if (randomItemRoll >= 70 && randomItemRoll < 90)
+                // 15% of 150
+                else if (randomItemRoll >= 70 && randomItemRoll < 85)
                 {
-                    Debug.Log("Chest Contained a 50 Screws");
+                    Debug.Log("Chest Contained a 150 Screws");
+                    GameManager.instance.screws += 150;
+
                 }
-                // 5% of medicine
-                else if (randomItemRoll >= 90 && randomItemRoll < 95)
+                // 10% of 175
+                else if (randomItemRoll >= 90 && randomItemRoll < 100)
                 {
-                    Debug.Log("Chest Contained a 75 Screws");
+                    Debug.Log("Chest Contained a 175 Screws");
+                    GameManager.instance.screws += 175;
+
                 }
-                // 5% chance of ultimate health object
+                // 5% chance of 200
                 else if (randomItemRoll >= 95 && randomItemRoll < 100)
                 {
-                    Debug.Log("Chest Contained a 100 Screws");
+                    Debug.Log("Chest Contained a 200 Screws");
+                    GameManager.instance.screws += 200;
                 }
 
 
